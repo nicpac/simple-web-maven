647efe409c437ca754ae28a6580015289e62667d
NicolasP <pacios.nicolas@gmail.com>
2019-01-30 10:36:33 +0100

    TEST 14

--- src/main/java/com/formation/HelloServlet.java
+++ src/main/java/com/formation/HelloServlet.java
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/hello")
-publidc class HelloServlet extends HttpServlet {
-	privsate static final long serialVersionUID = 1L;
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -19,7 +19,7 @@ publidc class HelloServlet extends HttpServlet {
 		writer.println("Hello from HelloServlet!");
 	}
 
-	protefcted void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
